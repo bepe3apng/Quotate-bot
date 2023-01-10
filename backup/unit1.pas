91e34c0aac5603edf78d52d87fb0a70257e5bdc2
bepe3apng <osharov2017@yandex.ru>
2023-01-10 19:36:28 +0800

    pre-build

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -43,20 +43,43 @@ implementation
 procedure TForm1.FormCreate(Sender: TObject);
 
 begin
+
   image1.Canvas.Brush.Color := clblack;
   image1.Canvas.Brush.Style := bsSolid;
-  image1.Canvas.rectangle(0,0,500,300);
+
   image1.Canvas.FloodFill(100,100,clblack,fsSurface);
   image1.Canvas.Font.Color:=clwhite;
   image1.Canvas.Font.size:=25;
   image1.Canvas.Font.Name:='Montserrat';
-  image1.Canvas.TextOut(75,15,'цитаты великих людей');
-  image1.Canvas.TextOut(60,60,'lfopjsfiops');
+
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
+var s,s1,s3:string;
+var a,s2:array of string;
+var j,i,len,x,y:integer;
 begin
-  clipboard.Assign(image1.Picture);
+  i:=0;
+  j:=0;
+  s:=edit1.text;
+ a:=s.Split(' ');
+ len:=length (s) div 15;
+ setlength(s2,len);
+ while j<(length(a)-1) do begin
+   while (length (s2[i]))<23 do begin
+   s2[i]:=s2[i]+' '+a[j];
+   if j=length(a)-1 then break;
+   inc(j);
+ end;
+ inc(i);
+ end;
+ image1.Canvas.TextOut(75,15,'Цитаты великих людей');
+ x:=75;
+ y:=90;
+for i:=0 to len-1 do begin
+image1.Canvas.TextOut(x,y,s2[i]);
+y:=y+45;
+end;
 end;
 
 procedure TForm1.Image1Click(Sender: TObject);
